807f6f08d425d333822c4199ed216c4df8f6a9d8
Steven Adel <stevenadel.dev@gmail.com>
2024-02-22 16:11:47 +0200

    Implement admin login controller

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -1,22 +1,30 @@
+const jwt = require('jsonwebtoken');
 const AdminModel = require('../models/admin.model');
 
 module.exports = {
-  async login(req, res) {
-    const user = req.body;
+  async login(req, res, next) {
+    const { username, password } = req.body;
 
     try {
-      const valid = await AdminModel.method(user);
-      res.json(valid);
+      const admin = await AdminModel.findOne({ username: `${username}` });
+
+      if (admin && await admin.verifyPassword(password)) {
+        const token = jwt.sign({ id: admin.id, username: admin.username }, process.env.SECRET_KEY, { expiresIn: '1d' });
+        return res.json({ token });
+      }
     } catch (err) {
-      res.status(400).json(err);
+      err.status = 400;
+      next(err);
     }
+
+    res.status(404).json({ error: 'Username or password incorrect.' });
   },
 
   async register(req, res, next) {
     const { username, password } = req.body;
 
     if (username && username.includes(' ')) {
-      return res.status(400).json({ error: 'Username should not contain spaces' });
+      return res.status(400).json({ error: 'Username cannot contain spaces.' });
     }
 
     try {
