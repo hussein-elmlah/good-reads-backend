f52182db271cd8a1ad764381de149583604a4031
hussein-elmlah <husseinelmlah11@gmail.com>
2024-02-22 16:27:49 +0200

    Replace router path '/users' with '/user'

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const categoriesRouter = require('./categories.routes');
 const booksRouter = require('./books.routes');
 const authorsRouter = require('./authors.routes');
 
-router.use('/users', userRouter);
+router.use('/user', userRouter);
 router.use('/admin', adminRouter);
 
 router.use('/categories', categoriesRouter);
