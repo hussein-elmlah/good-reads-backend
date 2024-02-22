84ce233fd92445d9233712d06c71575c5326594a
hussein-elmlah <husseinelmlah11@gmail.com>
2024-02-22 03:51:58 +0200

    Add errorHandler middleware for consistent error handling

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use((err, req, res, next) => {
-  res.status(err.status).json({ error: err.name, description: err.message });
-});
+
+app.use(errorHandler);
 
 const { PORT, DB_USERNAME, DB_PASSWORD, CLUSTER_URL, DB_NAME } = process.env;
 const uri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${CLUSTER_URL}/${DB_NAME}`;
