c751eb9307dff5814b72c8856e96986008d42470
unknown <https://github.com/Omnia-Mohamed88/>
2024-02-29 12:07:40 +0200

    fix

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const AdminController = require('../controllers/admin.controllers');
-const { authenticateAdmin } = require('../middlewares/authentication');
 
-router.post('/login', authenticateAdmin, AdminController.login);
+router.post('/login', AdminController.login);
 router.post('/register', AdminController.register);
 
 module.exports = router;
